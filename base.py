9e68f6f9ed8b12551b774733df569ec606e4d7f1
Egor Deev <67710823+IGlek@users.noreply.github.com>
2021-09-14 16:05:22 +0700

    Version 1.0

--- base.py
+++ base.py
@@ -1,13 +1,9 @@
 # ДАННЫЕ БОТА
-TOKEN = "1791246735:AAF3f6tb4T2Rk8atewn-2-2vQ6Q-DVy41Fc"
-
-# РИКРОЛЛ 
-RICKROLL_1 = 'https://vk.cc/8U7VuC' # https://www.youtube.com/watch?v=dQw4w9WgXcQ
-RICKROLL_2 = 'https://vk.cc/3A9NI7' # https://youtu.be/dQw4w9WgXcQ
+TOKEN = "your bot token"
 
 # ОБРАЩЕНИЯ К ПОЛЬЗОВАТЕЛЯМ
-KILL_LIST = ["побить", "отмудохать", "избить", "уебать", "отметелить"]
-TMOK_LIST = ["поцеловать", "чмокнуть", "соснуть", "лайкнуть"]
+KILL_LIST = ["побить", "отмудохать", "избить", "отметелить"]
+TMOK_LIST = ["поцеловать", "чмокнуть", "лайкнуть"]
 QUAT_LIST = [['дай', 'дайте'], ['ахуенный', 'ахуеный', 'охуеный', 'охуенный', 'блять', 'сука'], ['совет', 'совета']]
 
 # ФРАЗЫ ИВЕНТ ДРАКИ
@@ -25,63 +21,3 @@ TRU_ = ['Й', 'Ц', 'У', 'К', 'Е', 'Н', 'Г', 'Ш', 'Щ', 'З', 'Х', 'Ъ', 
         'Э', 'Я', 'Ч', 'С', 'М', 'И', 'Т', 'Ь', 'Б', 'Ю', ',']
 
 ALB = ['а', 'у', 'о', 'ы', 'и', 'э', 'я', 'ю', 'ё', 'е', 'А', 'У', 'О', 'Ы', 'И', 'Э', 'Я', 'Ю', 'Ё', 'Е']
-
-
-
-# НА ВСЯКИЙ СЛУЧАЙ
-
-'''
-# from filters import IsAdminFilter
-    # activate filters
-    dp.filters_factory.bind(IsAdminFilter)
-
-# await call.message.answer(str(randint(1, 10)))
-
-# await message.answer_sticker(r'CAACAgIAAxkBAAECmDFg8_fMoE0ZFAYxOh5KRyB2Y7VGZwACfQ0AAtMv4ElQpS-ysl5_6SAE') # ФУГА ДУМАЕТ
-
-# state = dp.current_state(chat=config.GROUP_ID, user=message.from_user.id)
-# await state.set_state(User.accepted)
-
-# ОСУЖДАЮ
-for word in base.BAN_LIST:
-    if word in txt:
-        await message.delete()
-        await message.answer("ОСУЖДАЮ")
-        print(name_group, ">>>", message.text)
-
-for word in base.BAN_NAME:
-    if word in txt and not "егор" in txt:
-        await message.delete()
-        await message.answer("Для обращений существует имя!")
-        print(name_group, ">>", message.text)
-
-# бан на минуту
-if len(original) == 2:
-    if "забанься" in txt:
-        await message.bot.restrict_chat_member(chat_id=id_group, user_id=[base.USERS[i][1] for i in base.USERS if i == no_pct.lower().split()[0]][0],
-                                               permissions=types.ChatPermissions(),
-                                               until_date=int(time()) + (31))
-        await message.reply(f'{message.from_user.first_name}, по вашему желанию отлетел дурачок на минуту)')
-
-# админ бан
-@dp.message_handler(is_admin=True, commands=['ban'], commands_prefix='!/')
-async def ban(message: types.Message):
-    if not message.reply_to_message:
-        await message.reply("Эта команда должна быть ответом на сообщение!")
-        return
-
-    await message.bot.delete_message(config.GROUP_ID, message.message_id)
-    await message.bot.kick_chat_member(chat_id=config.GROUP_ID, user_id=message.reply_to_message.from_user.id)
-
-    await message.bot.send_photo(chat_id=config.GROUP_ID, photo="1.jpg", caption="Пользователь унижен!\nПоздравляю ;)")
-
-# чекер данных фото
-@dp.message_handler(content_types=['photo'])
-async def scan_message(msg: types.Message):
-    document_id = msg.photo[0].file_id
-    file_info = await bot.get_file(document_id)
-    print(f'file_id: {file_info.file_id}')
-    print(f'file_path: {file_info.file_path}')
-    print(f'file_size: {file_info.file_size}')
-    print(f'file_unique_id: {file_info.file_unique_id}')
-'''
